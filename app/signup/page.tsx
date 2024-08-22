3745ef7fe2f27c6f12ee3aacae53a9d400a071df
Nguyen Nam Duong <nguyennamduong1212@gmail.com>
2024-08-22 11:15:03 +0700

    blog-frontend base

---
+++ app/signup/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignupForm from '@/components/SignUpForm'
+
+const Signup = () => {
+  return (
+    <div>
+      <SignupForm />
+    </div>
+  )
+}
+
+export default Signup
